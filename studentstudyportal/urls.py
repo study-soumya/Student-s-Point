2942c2d471dfd240e9676656348a307ab87c5932
Soumya Patra <sonusoum.study@gmail.com>
2023-11-15 00:59:37 +0530

    first commit

--- studentstudyportal/urls.py
+++ studentstudyportal/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from dashboard import views as dash_views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +29,4 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='dashboard/logout.html'), name='logout'),
     path('profile/', dash_views.profile, name='profile'),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
